1565ce610ffe9f1c97217be66ada15a8de9c784f
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-11-04 16:13:29 -0400

    Finish a component

--- frontend/src/components/QuizQuestion.jsx
+++ frontend/src/components/QuizQuestion.jsx
@@ -3,6 +3,13 @@ import { Button } from "@/components/ui/button";
 
 function QuizQuestion({ question, answerArr, id, correctAns }) {
   const [correct, setCorrect] = useState(false);
+  const [selectionArr, setSelectionArr] = useState(() => Array(answerArr.length).fill(false));
+
+  const handleSelection = (index) => {
+    const newSelectionArr = Array(answerArr.length).fill(false);
+    newSelectionArr[index] = true;
+    setSelectionArr(newSelectionArr);
+  }
 
   return (
     <>
@@ -12,7 +19,13 @@ function QuizQuestion({ question, answerArr, id, correctAns }) {
         </h2>
         <p className="text-base text-gray-600 dark:text-gray-300">{question}</p>
         {answerArr.map((answer, index) => (
-          <div className="flex flex-row rounded-md border items-center p-4 gap-4">
+          <div
+            className={`flex flex-row rounded-md border items-center p-4 gap-4 ${
+              selectionArr[index] ? 'border-blue-500' : 'border-gray-300'
+            }`}
+            key={index}
+            onClick={() => handleSelection(index)}
+          >
             <label>
               {answer}
             </label>
