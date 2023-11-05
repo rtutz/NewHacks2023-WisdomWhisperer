6ebc757fd635347dbe8fe5c06b6db03fe8a72532
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-11-04 20:48:46 -0400

    Merge conflict resolutions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import Banner from './components/Banner'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
-import AudioInput from './components/AudioInput';
 
 function App() {
   return (
     <>
-      <div className='flex flex-col justify-center items-center gap-4'>
-        <AudioInput />
-      </div>
-      {/* <Router>
+      {/* <div className='flex flex-col justify-center items-center gap-4'>
+        <QuizQuestion question={"Prove this transformation is linear"} answerArr={["Answer 1", "Answer 3"]} id={1} correctAns={"Answer 2"}/>
+      </div> */}
+      <Router>
         <Routes>
           <Route exact path='/' element={<Banner/>}/>
           <Route path='/home' element={<Home/>}/>
         </Routes>
-      </Router> */}
+      </Router>
     </>
   );
 }
