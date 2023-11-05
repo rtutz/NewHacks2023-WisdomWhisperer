fed58c4f47e8b0df1b5c1659d4799275a0f9cba9
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-11-05 06:53:59 -0500

    Update whisp.py

--- backended/routes/whisp.py
+++ backended/routes/whisp.py
@@ -191,7 +191,7 @@ def notes():
     for item in videos:
         video_info = redis_client.hgetall(item)
         # print(video_info)
-        return_array.append({str(item, encoding='utf-8'): {"title": str(video_info.get(b'title'), encoding='utf-8'), "summary": str(video_info.get(b'title'), encoding='utf-8')}})
+        return_array.append({str(item, encoding='utf-8'): {"title": str(video_info.get(b'title'), encoding='utf-8'), "summary": str(video_info.get(b'summary'), encoding='utf-8')}})
         print({str(item, encoding='utf-8'): {"title": str(video_info.get(b'title'), encoding='utf-8')}})
         i += 1
     redis_client.close()
