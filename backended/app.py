51271a5e7b62a86c51782b7325802b05fb5693b2
James Liang <86000097+jamesliangg@users.noreply.github.com>
2023-11-05 00:47:37 -0400

    Fixed Flask

--- backended/app.py
+++ backended/app.py
@@ -19,4 +19,4 @@ app.register_blueprint(chat, url_prefix='/chat')  # Registering the Blueprint
 
 # https://stackoverflow.com/questions/64218171/getting-a-socket-hang-up-error-when-trying-to-access-my-flask-app-in-a-docker-co
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=False, port=5555)
