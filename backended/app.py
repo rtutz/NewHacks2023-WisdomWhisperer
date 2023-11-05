29357576ff87de8f687171098588b0dbc5b54653
Ri-Hong <riri.hong@gmail.com>
2023-11-05 00:43:30 -0400

    LLM works?

--- backended/app.py
+++ backended/app.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 
 import dotenv
+
+from routes.chat import chat  # Importing the Blueprint
+from routes.whisp import whisp
+
 dotenv.load_dotenv()
 
-# from routes.chat import chat  # Importing the Blueprint
 from routes.whisp import whisp
 
 app = Flask(__name__)
@@ -12,7 +15,8 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:5555"}})  # Allow only
 
 
 app.register_blueprint(whisp, url_prefix='/v1')  # Registering the Blueprint
+app.register_blueprint(chat, url_prefix='/chat')  # Registering the Blueprint
 
 # https://stackoverflow.com/questions/64218171/getting-a-socket-hang-up-error-when-trying-to-access-my-flask-app-in-a-docker-co
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False, port=5555)
+    app.run()
--- backended/routes/chat.py
+++ backended/routes/chat.py
@@ -1,11 +1,11 @@
 import os
+import redis
 
 # GPC Vertex AI Stuff
 from langchain.llms import VertexAI
 from langchain.embeddings import VertexAIEmbeddings
 
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+from langchain.vectorstores import Redis
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
@@ -19,19 +19,27 @@ from flask_jwt_extended import jwt_required
 chat = Blueprint('chat', __name__)
 
 dotenv.load_dotenv()
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../../GACKey.json"
 
-
-# The Chroma storage is not in the routes directory, it's out by 1
 current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-persist_directory = os.path.join(parent_dir, "storage")
+key_path = os.path.join(current_dir, "GACKey.json")
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
 
 
 embeddings = VertexAIEmbeddings()
 
-vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
-retriever = vectordb.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+rds = Redis.from_texts(  
+    ["foo"],  
+    embeddings,  
+    redis_url=os.getenv("REDIS_URL"),  
+    index_name="topic1",  
+)  
+
+rds.write_schema("redis_schema.yaml")
+
+# vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+retriever = rds.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
 
 # qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 global memory
@@ -45,6 +53,22 @@ qa = ConversationalRetrievalChain.from_llm(
     # return_generated_question = True
 )
 
+# create_index is called when a new course is created
+def create_index(course_id, texts, embeddings, metadata):
+    index_name = f'course_{course_id}'
+    redis_url = "redis://localhost:6379"  # replace with your Redis URL
+    rds = Redis.from_texts(
+        texts,
+        embeddings,
+        redis_url=redis_url,
+        index_name=index_name
+    )
+    return rds  # return the Redis instance for further use
+
+def insert_embeddings(rds, texts, embeddings):
+    documents = [{"text": text, "embedding": embedding} for text, embedding in zip(texts, embeddings)]
+    rds.add_documents(documents)
+
 
 def handle_query(user_input):
     query = f"###Prompt {user_input}"
@@ -57,7 +81,7 @@ def handle_query(user_input):
 
 
 @chat.route('/get-response', methods=['POST'])
-@jwt_required()
+# @jwt_required()
 def get_response():
     data = request.get_json()
     query = data.get('query')
@@ -68,7 +92,7 @@ def get_response():
 
 
 @chat.route('/clear-memory', methods=['POST'])
-@jwt_required()
+# @jwt_required()
 def clear_chat():
     global memory  # Access the global memory variable
     memory.clear()  # Clear the memory
