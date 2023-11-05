8977eba6884e25aba4053c8edac3c337f51cd020
rtutz <ronaldjohn.tutor@gmail.com>
2023-11-05 08:49:36 -0500

    fixed quiz UI

--- frontend/src/components/QuizHome.jsx
+++ frontend/src/components/QuizHome.jsx
@@ -1,17 +1,42 @@
 import SideBar from "./SideBar";
 import QuizQuestion from "./QuizQuestion";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom'
+import axios from 'axios';
 function QuizHome() {
-    const [currentQ, setcurrentQ] = useState(1);
+    const location = useLocation()
+    let path = location.pathname; // "/home/C1/pENQyjXkcw4"
+    let segments = path.split("/"); // splits the path into an array of segments
+    segments.pop(); // retrieves the last element of the array
+    let courseID = segments.pop(); // retrieves the last element of the array
+
+    const [data, setData] = useState(null);
+    const endpoint = "https://www.youtube.com/"
+  
+    useEffect(() => {
+        // Send a POST request 
+        axios.post(endpoint, { courseID })
+          .then(response => {
+            const [question, choices, answer] = response.data;
+            
+            // If data is retrieved successfully, set it to the state
+            setData({ question, choices, answer });
+          })
+          .catch(error => {
+            // Error handling
+            console.error('Something went wrong!', error);
+          });
+      }, [courseID]);
+
     return (
         <div className="flex">
             <SideBar/>
             <div className="w-full flex justify-center">
                 <QuizQuestion 
-                question="What is the powerhouse of the cell?"
-                answerArr={["Cytoplasm", "Mitochondria", "Dogs", "Cats"]}
+                question={data.question}
+                answerArr={data.choices}
                 id={1}
-                correctAns={"Mitochondria"}/>
+                correctAns={data.answer}/>
             </div>
             
         </div>
